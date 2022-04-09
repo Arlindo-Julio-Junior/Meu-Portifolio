41bc9392b094a4a8df12bf4ec4d84d8f382c0149
Arlindo-Julio-Junior <arlindojrti@gmail.com>
2022-04-09 17:22:45 +0100

    Animação em javascript

--- static/script.js
+++ static/script.js
@@ -23,3 +23,25 @@ navItem.forEach(item => {
         }
     })
 })
+
+//Animar todos os items na tela que tiverem meu atributo data-anime
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+    
+    item.forEach((Element) => {
+        if (windowTop > Element.offsetTop) {
+            Element.classList.add("animate");
+        } else {
+            Element.classList.remove("animate");
+        }
+    } )
+};
+
+animeScroll();
+
+window.addEventListener("scroll", ()=>{
+    animeScroll();
+})
